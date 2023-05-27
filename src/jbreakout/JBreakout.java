a5960889f2bf5654a3b9afdc67077d14c2359050
mpp <mpp0xdd@gmail.com>
2023-05-27 19:04:11 +0900

    JBreakoutWindowを削除

--- src/jbreakout/JBreakout.java
+++ src/jbreakout/JBreakout.java
@@ -5,7 +5,7 @@ import jglib.component.GameWindow;
 
 public class JBreakout {
   public static void main(String[] args) {
-    GameWindow gameWindow = new JBreakoutWindow();
+    GameWindow gameWindow = new GameWindow("JBreakout");
     MainScreen mainScreen = new MainScreen();
 
     gameWindow.switchGameScreen(mainScreen);
--- src/jbreakout/JBreakoutWindow.java
+++
@@ -1,9 +0,0 @@
-package jbreakout;
-
-import jglib.component.GameWindow;
-
-public class JBreakoutWindow extends GameWindow {
-  public JBreakoutWindow() {
-    super("JBreakout");
-  }
-}
