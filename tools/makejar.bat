9a2a1eeee485af753339552610751f284144f6b3
mpp <mpp0xdd@gmail.com>
2023-05-27 22:51:28 +0900

    不要になったスクリプトファイルを削除

--- tools/makejar.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set JGLIB=..\JGLib
-set CLASSES=classes
-set MAIN=JBreakout
-
-del "%CLASSES%\*.class"
-javac -d "%CLASSES%" -cp "%JGLIB%;." -encoding UTF-8 *.java
-jar cvfe "%MAIN%.jar" "%MAIN%" -C %CLASSES% .
--- tools/makejar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CFLAGS="-J-Dfile.encoding=UTF-8"
-JGLIB="../JGLib"
-CLASSES="classes"
-MAIN="JBreakout"
-
-rm -f "$CLASSES"/*.class
-javac -d "$CLASSES" "$CFLAGS" -cp "${JGLIB}:." *.java
-jar "$CFLAGS" cvfe "${MAIN}.jar" "$MAIN" -C "$CLASSES" .
