a5a5b383dfcb48254aa07227fa234c602f7a5144
mpp <mpp0xdd@gmail.com>
2023-07-09 18:17:57 +0900

    Revert "ロードに成功した音の数と宣言済みの音の数の情報を得れるように"
    This reverts commit 2fe225e956bfc27a67478869aef86a836ba731b3.

--- src/jbreakout/common/SoundFactory.java
+++ src/jbreakout/common/SoundFactory.java
@@ -1,11 +1,5 @@
 package jbreakout.common;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.lang.reflect.ParameterizedType;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 import javax.sound.sampled.Clip;
 import jglib.util.GameUtilities;
@@ -15,8 +9,6 @@ public final class SoundFactory {
   private SoundFactory() {
     // restrict instantiation
   }
-
-  private static final List<Optional<Clip>> declaredSounds;
 
   private static final Optional<Clip> mainScreenBgmClip;
   private static final Optional<Clip> ballFallClip;
@@ -42,47 +34,6 @@ public final class SoundFactory {
     paddleReboundClip =
         GameUtilities.loadClip(SoundFactory.class.getResource("sounds/paddle_rebound.wav"));
     paddleReboundClip.ifPresent(clip -> GameUtilities.setVolume(clip, 0.75f));
-  }
-
-  static {
-    ArrayList<Optional<Clip>> _declaredSounds = new ArrayList<>();
-    for (Field field : SoundFactory.class.getDeclaredFields()) {
-      if (!Modifier.isPrivate(field.getModifiers())) {
-        continue;
-      }
-      if (!Modifier.isStatic(field.getModifiers())) {
-        continue;
-      }
-      if (!Modifier.isFinal(field.getModifiers())) {
-        continue;
-      }
-      if (!field.getType().equals(Optional.class)) {
-        continue;
-      }
-      if (!(field.getGenericType() instanceof ParameterizedType)) {
-        continue;
-      }
-      if (!((ParameterizedType) field.getGenericType())
-          .getActualTypeArguments()[0].equals(Clip.class)) {
-        continue;
-      }
-      try {
-        @SuppressWarnings("unchecked")
-        Optional<Clip> optional = (Optional<Clip>) field.get(null);
-        _declaredSounds.add(optional);
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    }
-    declaredSounds = Collections.unmodifiableList(_declaredSounds);
-  }
-
-  public static List<Optional<Clip>> declaredSounds() {
-    return declaredSounds;
-  }
-
-  public static List<Optional<Clip>> availableSounds() {
-    return declaredSounds.stream().filter(Optional::isPresent).toList();
   }
 
   public static Optional<Clip> mainScreenBgmClip() {
