2fe225e956bfc27a67478869aef86a836ba731b3
mpp <mpp0xdd@gmail.com>
2023-07-09 15:17:38 +0900

    ロードに成功した音の数と宣言済みの音の数の情報を得れるように

--- src/jbreakout/JBreakout.java
+++ src/jbreakout/JBreakout.java
@@ -1,5 +1,6 @@
 package jbreakout;
 
+import jbreakout.common.SoundFactory;
 import jbreakout.screen.MainScreen;
 import jglib.component.GameWindow;
 
@@ -7,6 +8,10 @@ public class JBreakout {
   public static void main(String[] args) {
     GameWindow gameWindow = new GameWindow("JBreakout");
     MainScreen mainScreen = new MainScreen();
+
+    int available = SoundFactory.availableSounds().size();
+    int declared = SoundFactory.declaredSounds().size();
+    System.err.printf("Available:%s, Declared:%s%n", available, declared);
 
     gameWindow.switchGameScreen(mainScreen);
     gameWindow.setVisible(true);
--- src/jbreakout/common/SoundFactory.java
+++ src/jbreakout/common/SoundFactory.java
@@ -1,5 +1,11 @@
 package jbreakout.common;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import javax.sound.sampled.Clip;
 import jglib.util.GameUtilities;
@@ -9,6 +15,8 @@ public final class SoundFactory {
   private SoundFactory() {
     // restrict instantiation
   }
+
+  private static final List<Optional<Clip>> declaredSounds;
 
   private static final Optional<Clip> mainScreenBgmClip;
   private static final Optional<Clip> ballFallClip;
@@ -34,6 +42,47 @@ public final class SoundFactory {
     paddleReboundClip =
         GameUtilities.loadClip(SoundFactory.class.getResource("sounds/paddle_rebound.wav"));
     paddleReboundClip.ifPresent(clip -> GameUtilities.setVolume(clip, 0.75f));
+  }
+
+  static {
+    ArrayList<Optional<Clip>> _declaredSounds = new ArrayList<>();
+    for (Field field : SoundFactory.class.getDeclaredFields()) {
+      if (!Modifier.isPrivate(field.getModifiers())) {
+        continue;
+      }
+      if (!Modifier.isStatic(field.getModifiers())) {
+        continue;
+      }
+      if (!Modifier.isFinal(field.getModifiers())) {
+        continue;
+      }
+      if (!field.getType().equals(Optional.class)) {
+        continue;
+      }
+      if (!(field.getGenericType() instanceof ParameterizedType)) {
+        continue;
+      }
+      if (!((ParameterizedType) field.getGenericType())
+          .getActualTypeArguments()[0].equals(Clip.class)) {
+        continue;
+      }
+      try {
+        @SuppressWarnings("unchecked")
+        Optional<Clip> optional = (Optional<Clip>) field.get(null);
+        _declaredSounds.add(optional);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    declaredSounds = Collections.unmodifiableList(_declaredSounds);
+  }
+
+  public static List<Optional<Clip>> declaredSounds() {
+    return declaredSounds;
+  }
+
+  public static List<Optional<Clip>> availableSounds() {
+    return declaredSounds.stream().filter(Optional::isPresent).toList();
   }
 
   public static Optional<Clip> mainScreenBgmClip() {
