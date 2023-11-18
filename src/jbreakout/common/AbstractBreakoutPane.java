ac7a82c6353264385136a5ab9b547c0e32d4f9f8
mpp <mpp0xdd@gmail.com>
2023-11-18 11:09:42 +0900

    AbstractBreakoutPaneを修正
    update()とdraw()の実装を具体クラスに移動

--- src/jbreakout/common/AbstractBreakoutPane.java
+++ src/jbreakout/common/AbstractBreakoutPane.java
@@ -1,28 +1,11 @@
 package jbreakout.common;
 
 import static jbreakout.common.Constants.BALL_RELOCATION_INTERVAL;
-import static jbreakout.common.Constants.COLOR_OF_DRAWING_STRING;
-import static jbreakout.common.Constants.CURRENT_ROUND_DRAWING_AREA_X;
-import static jbreakout.common.Constants.CURRENT_ROUND_DRAWING_AREA_Y;
-import static jbreakout.common.Constants.CURRENT_TURN_DRAWING_AREA_X;
-import static jbreakout.common.Constants.CURRENT_TURN_DRAWING_AREA_Y;
-import static jbreakout.common.Constants.FONT_OF_DRAWING_STRING;
-import static jbreakout.common.Constants.GAME_ROUNDS;
-import static jbreakout.common.Constants.PLAYER_MAX_TURNS;
-import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.Point;
 import java.util.Timer;
 import java.util.TimerTask;
-import jglib.util.GameUtilities;
 
 public abstract class AbstractBreakoutPane implements Drawable {
-  private boolean isGameOver = false;
-  private int currentNumOfBricksEliminated = 0;
-  private int currentRound = 1;
-  private int currentScore = 0;
-  private int currentTurn = 1;
-  private int currentTotalScore = 0;
 
   private Brick[] bricks;
   private Ball ball;
@@ -39,62 +22,6 @@ public abstract class AbstractBreakoutPane implements Drawable {
     relocateBall();
   }
 
-  public void update() {
-    if (!ball.isVisible()) {
-      return;
-    }
-
-    ball.move();
-    if (ball.getX() < 0) {
-      ball.bounceX();
-      ball.setX(0);
-    }
-    if (ball.getX() + ball.size() > width()) {
-      ball.bounceX();
-      ball.setX(width() - ball.size());
-    }
-    if (ball.getY() < 0) {
-      ball.bounceY();
-      ball.setY(0);
-    }
-    // ボールをパドルで取りそこなったとき
-    if (ball.getY() > height()) {
-      // fallClip.ifPresent(GameUtilities::playClip);
-      ball.setVisible(false);
-      if (currentTurn == PLAYER_MAX_TURNS) {
-        isGameOver = true;
-      } else {
-        currentTurn++;
-        relocateBall();
-      }
-      return;
-    }
-
-    paddle.rebound(ball);
-
-    for (Brick brick : bricks) {
-      if (brick.rebound(ball) != ball) {
-        continue;
-      }
-
-      currentScore += brick.score();
-      brick.eliminate();
-      currentNumOfBricksEliminated++;
-      if (currentNumOfBricksEliminated == bricks.length) {
-        currentTotalScore += currentScore;
-        if (currentRound == GAME_ROUNDS) {
-          isGameOver = true;
-        } else {
-          currentRound++;
-          currentScore = 0;
-          currentNumOfBricksEliminated = 0;
-          startRound();
-        }
-      }
-      break;
-    }
-  }
-
   public void movePaddle(int x) {
     int halfWidthOfPaddle = paddle.width() / 2;
     if (halfWidthOfPaddle <= x && x <= width() - halfWidthOfPaddle) {
@@ -102,40 +29,33 @@ public abstract class AbstractBreakoutPane implements Drawable {
     }
   }
 
-  @Override
-  public void draw(Graphics g) {
-    g.setColor(Color.BLACK);
-    g.fillRect(0, 0, width(), height());
-
-    g.setColor(COLOR_OF_DRAWING_STRING);
-    g.setFont(FONT_OF_DRAWING_STRING);
-    GameUtilities.drawString(
-        g,
-        CURRENT_ROUND_DRAWING_AREA_X,
-        CURRENT_ROUND_DRAWING_AREA_Y,
-        String.valueOf(currentRound),
-        String.format(" %03d", currentScore));
-    GameUtilities.drawStringFromTopRight(
-        g,
-        CURRENT_TURN_DRAWING_AREA_X,
-        CURRENT_TURN_DRAWING_AREA_Y,
-        String.format("%d     ", currentTurn),
-        String.format(" %03d", currentTotalScore));
-
-    for (Brick brick : bricks) {
-      brick.draw(g);
-    }
-
-    ball.draw(g);
-
-    paddle.draw(g);
-
-    if (isGameOver) {
-      g.setColor(COLOR_OF_DRAWING_STRING);
-      g.setFont(FONT_OF_DRAWING_STRING);
-      GameUtilities.drawStringAfterCentering(g, width() / 2, height() / 2, "Game Over!");
-    }
+  protected Brick[] bricks() {
+    return bricks;
   }
+
+  protected Ball ball() {
+    return ball;
+  }
+
+  protected Paddle paddle() {
+    return paddle;
+  }
+
+  protected void relocateBall() {
+    this.ball = ballFactory().createBall(ballVelocity(), ballPoint());
+    Timer roundTimer = new Timer();
+    roundTimer.schedule(
+        new TimerTask() {
+          @Override
+          public void run() {
+            ball.setVisible(true);
+            roundTimer.cancel();
+          }
+        },
+        BALL_RELOCATION_INTERVAL);
+  }
+
+  public abstract void update();
 
   public abstract int width();
 
@@ -163,19 +83,5 @@ public abstract class AbstractBreakoutPane implements Drawable {
     final int y = bricksY + numOfBrickRows * (brickHeight + bricksMargin);
 
     return new Point(x, y);
-  }
-
-  private void relocateBall() {
-    this.ball = ballFactory().createBall(ballVelocity(), ballPoint());
-    Timer roundTimer = new Timer();
-    roundTimer.schedule(
-        new TimerTask() {
-          @Override
-          public void run() {
-            ball.setVisible(true);
-            roundTimer.cancel();
-          }
-        },
-        BALL_RELOCATION_INTERVAL);
   }
 }
--- src/jbreakout/component/BreakoutPane.java
+++ src/jbreakout/component/BreakoutPane.java
@@ -1,13 +1,124 @@
 package jbreakout.component;
 
+import static jbreakout.common.Constants.COLOR_OF_DRAWING_STRING;
+import static jbreakout.common.Constants.CURRENT_ROUND_DRAWING_AREA_X;
+import static jbreakout.common.Constants.CURRENT_ROUND_DRAWING_AREA_Y;
+import static jbreakout.common.Constants.CURRENT_TURN_DRAWING_AREA_X;
+import static jbreakout.common.Constants.CURRENT_TURN_DRAWING_AREA_Y;
+import static jbreakout.common.Constants.FONT_OF_DRAWING_STRING;
+import static jbreakout.common.Constants.GAME_ROUNDS;
+import static jbreakout.common.Constants.PLAYER_MAX_TURNS;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Point;
 import jbreakout.common.AbstractBallFactory;
 import jbreakout.common.AbstractBreakoutPane;
 import jbreakout.common.AbstractBrickFactory;
 import jbreakout.common.AbstractPaddleFactory;
+import jbreakout.common.Brick;
 import jbreakout.common.Velocity;
+import jglib.util.GameUtilities;
 
 public class BreakoutPane extends AbstractBreakoutPane {
+
+  private boolean isGameOver = false;
+  private int currentNumOfBricksEliminated = 0;
+  private int currentRound = 1;
+  private int currentScore = 0;
+  private int currentTurn = 1;
+  private int currentTotalScore = 0;
+
+  @Override
+  public void update() {
+    if (!ball().isVisible()) {
+      return;
+    }
+
+    ball().move();
+    if (ball().getX() < 0) {
+      ball().bounceX();
+      ball().setX(0);
+    }
+    if (ball().getX() + ball().size() > width()) {
+      ball().bounceX();
+      ball().setX(width() - ball().size());
+    }
+    if (ball().getY() < 0) {
+      ball().bounceY();
+      ball().setY(0);
+    }
+    // ボールをパドルで取りそこなったとき
+    if (ball().getY() > height()) {
+      // fallClip.ifPresent(GameUtilities::playClip);
+      ball().setVisible(false);
+      if (currentTurn == PLAYER_MAX_TURNS) {
+        isGameOver = true;
+      } else {
+        currentTurn++;
+        relocateBall();
+      }
+      return;
+    }
+
+    paddle().rebound(ball());
+
+    for (Brick brick : bricks()) {
+      if (brick.rebound(ball()) != ball()) {
+        continue;
+      }
+
+      currentScore += brick.score();
+      brick.eliminate();
+      currentNumOfBricksEliminated++;
+      if (currentNumOfBricksEliminated == bricks().length) {
+        currentTotalScore += currentScore;
+        if (currentRound == GAME_ROUNDS) {
+          isGameOver = true;
+        } else {
+          currentRound++;
+          currentScore = 0;
+          currentNumOfBricksEliminated = 0;
+          startRound();
+        }
+      }
+      break;
+    }
+  }
+
+  @Override
+  public void draw(Graphics g) {
+    g.setColor(Color.BLACK);
+    g.fillRect(0, 0, width(), height());
+
+    g.setColor(COLOR_OF_DRAWING_STRING);
+    g.setFont(FONT_OF_DRAWING_STRING);
+    GameUtilities.drawString(
+        g,
+        CURRENT_ROUND_DRAWING_AREA_X,
+        CURRENT_ROUND_DRAWING_AREA_Y,
+        String.valueOf(currentRound),
+        String.format(" %03d", currentScore));
+    GameUtilities.drawStringFromTopRight(
+        g,
+        CURRENT_TURN_DRAWING_AREA_X,
+        CURRENT_TURN_DRAWING_AREA_Y,
+        String.format("%d     ", currentTurn),
+        String.format(" %03d", currentTotalScore));
+
+    for (Brick brick : bricks()) {
+      brick.draw(g);
+    }
+
+    ball().draw(g);
+
+    paddle().draw(g);
+
+    if (isGameOver) {
+      g.setColor(COLOR_OF_DRAWING_STRING);
+      g.setFont(FONT_OF_DRAWING_STRING);
+      GameUtilities.drawStringAfterCentering(g, width() / 2, height() / 2, "Game Over!");
+    }
+  }
 
   @Override
   public int width() {
