f82e7827311bf19a634e73487d810e09d1e071bb
mpp <mpp0xdd@gmail.com>
2023-11-19 20:09:06 +0900

    WIP

--- src/jbreakout/factory/AbstractBallFactory.java
+++ src/jbreakout/factory/AbstractBallFactory.java
@@ -5,6 +5,7 @@ import jbreakout.common.Ball;
 import jbreakout.common.Velocity;
 
 public abstract class AbstractBallFactory<T extends Ball> {
+
   public abstract T createBall(Velocity velocity, Point location);
 
   public abstract int size();
--- src/jbreakout/factory/AbstractPaddleFactory.java
+++ src/jbreakout/factory/AbstractPaddleFactory.java
@@ -4,6 +4,7 @@ import java.awt.Point;
 import jbreakout.common.Paddle;
 
 public abstract class AbstractPaddleFactory<T extends Paddle> {
+
   public abstract T createPaddle(Point point);
 
   public abstract int width();
