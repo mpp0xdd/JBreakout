a276e309bc3bc52b75f8ea5f3e55a47ebeb542b8
mpp <mpp0xdd@gmail.com>
2024-06-18 22:03:31 +0900

    WIP

--- src/jbreakout/JBreakout.java
+++ src/jbreakout/JBreakout.java
@@ -2,10 +2,17 @@ package jbreakout;
 
 import jbreakout.component.DefaultBreakoutPane;
 import jbreakout.screen.MainScreen;
+import jglib.base.Game;
 import jglib.component.GameWindow;
 
-public class JBreakout {
+public class JBreakout extends Game {
+
   public static void main(String[] args) {
+    launch(JBreakout.class);
+  }
+
+  @Override
+  protected void start() {
     GameWindow gameWindow = new GameWindow("JBreakout");
     MainScreen mainScreen = new MainScreen(new DefaultBreakoutPane());
 
